6a2f28b11c440ee89e6866c9811a72a8e52aeb32
e-manarang <31264507+e-manarang@users.noreply.github.com>
2020-08-16 21:39:02 -0600

    Missing files.

---
+++ ConnectFour/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Title = "Connect Four";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("==============");
+            Console.WriteLine(" Connect Four ");
+            Console.WriteLine("==============");
+
+            ConnectFour game;
+            string play;
+
+            do
+            {
+                game = new ConnectFour();
+                game.NewGame();
+                do
+                {
+                    Console.Write("Play Again [Y/N]? ");
+                    play = Console.ReadLine().Trim().ToUpper();
+
+                    if (play.Length == 0)
+                        play = "X";
+                }
+                while (play[0] != 'Y' && play[0] != 'N');
+                
+            }
+            while (play[0] == 'Y');
+        }
+    }
+}
---
+++ ConnectFour/Values/Values.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// This file handles any enumerations and constants used for the game.
+/// </summary>
+namespace ConnectFour
+{
+    /// <summary>
+    /// ComputerDifficulty defines the level of difficulty of the Computer player.
+    /// </summary>
+    public enum ComputerDifficulty : byte
+    {
+        Random = 0,
+        Easy,
+        Normal,
+        Advanced
+    }
+
+    /// <summary>
+    /// Pertains to which part of the board the display needs to display.
+    /// </summary>
+    public enum BoardBorder : byte
+    {
+        Top = 0,
+        Middle,
+        Bottom
+    }
+
+    public enum Direction: byte
+    {
+        Up = 0,
+        Left,
+        Right,
+        Down,
+        UpLeft,
+        UpRight,
+        DownLeft,
+        DownRight
+    }
+}
